da5e9d80c348a052c037d9a92268cf2f9565e2db
Carlos del Prado <carlos.delpradomota@telefonica.com>
2022-03-01 19:25:55 +0000

    feat: error handling

---
+++ api/config/routes.config.js
@@ -0,0 +1,24 @@
+const createError = require('http-errors');
+const express = require('express');
+const router = express.Router();
+
+//router.get()
+//router.get()
+//router.get()
+//router.get()
+//router.get()
+//router.get()
+
+router.use((req, res, next) => next(createError(404, 'Route not found')));
+
+router.use((error, req, res, next) => {
+  console.error(error);
+  
+  const data = {
+    message: error.message
+  };
+
+  res.status(error.status).json(data);
+})
+
+module.exports = router;
